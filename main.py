9d23eb3178c2364360b9a474cc92b5ccd12b503f
Jawwad Khan <jawwadk.22@gmail.com>
2023-12-14 17:32:57 +0100

    Created main.py. Pipe Install pandas and numpy. Added training set loader.

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Decemeber 14 17:31:18 2023
+
+@author: Jawwad Khan, 7417247, Thesis Cybersecurity, Title: The Role of the Adversary's Success Rate Metric in Cybersecurity
+"""
+
+import pandas as pd
+import numpy as np
+
+class File:
+
+    def load_training_df(self):
+        # Load the training dataset
+        self.df = pd.read_csv()
+        return self.df
