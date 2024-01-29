64ad3aab978c2d25aff943cd7902f2907836dd5a
Jawwad Khan <jawwadk.22@gmail.com>
2024-01-29 20:01:59 +0100

    bugfixes

--- deanonymization/algorithm1b.py
+++ deanonymization/algorithm1b.py
@@ -4,7 +4,7 @@ Created on Thu January 14 17:31:18 2023
 
 @author:Jawwad Khan,7417247,Thesis Cybersecurity,Title: The Role of the Adversary's Success Rate Metric in Cybersecurity
 """
-
+import pandas
 import pandas as pd
 import numpy as np
 from collections import Counter as ct
@@ -22,16 +22,16 @@ def score_function(aux, record, counts):
     score = 0
     rho0 = 1.5
     d0 = 30
-
     rating = float(record.get(1))
     timestamp = float(record.get(2))
-
     aux_rating = float(aux.get(2))
     aux_timestamp = float(aux.get(3))
 
-    wt = (1/(np.log10(counts)))
-
-    score = wt * (np.exp(-((abs(aux_rating - rating)) / rho0)) + np.exp(-((abs(aux_timestamp - timestamp)) / d0)))
+    if aux_timestamp < 0:
+        score = 0
+    else:
+        wt = (1/(np.log10(counts)))
+        score = wt * (np.exp(-((abs(aux_rating - rating)) / rho0)) + np.exp(-((abs(aux_timestamp - timestamp)) / d0)))
 
     return score
 
@@ -45,21 +45,24 @@ def matching_criterion(scores, eccentricity):
     Returns:
     bool: True if there is a match, False otherwise.
     """
-    sorted_scores = sorted(scores, reverse=True)
-    #print(sorted_scores, "sorted scores")
-    unique_list = list(sorted(set(sorted_scores), reverse=True))
-    #print(unique_list, "unique List")
-    max_score = unique_list[0]
-    #print(max_score, "S1")
-    max2_score = unique_list[1]
-    #print(max2_score, "S2")
-    sigma = np.std(scores)
-    #print(sigma, "sigma")
-    #print((max_score - max2_score) / sigma, "eccentricity")
-    if (max_score - max2_score) / sigma < eccentricity:
-        return False, max_score, ((max_score - max2_score) / sigma)
+    if (sum(scores) == 0):
+        return False, 0, 0
     else:
-        return True, max_score, ((max_score - max2_score) / sigma)
+        sorted_scores = sorted(scores, reverse=True)
+        #print(sorted_scores, "sorted scores")
+        unique_list = list(sorted(set(sorted_scores), reverse=True))
+        #print(unique_list, "unique List")
+        max_score = unique_list[0]
+        #print(max_score, "S1")
+        max2_score = unique_list[1]
+        #print(max2_score, "S2")
+        sigma = np.std(scores)
+        #print(sigma, "sigma")
+        #print((max_score - max2_score) / sigma, "eccentricity")
+        if (max_score - max2_score) / sigma < eccentricity:
+            return False, max_score, ((max_score - max2_score) / sigma)
+        else:
+            return True, max_score, ((max_score - max2_score) / sigma)
 
 def record_selection(scores):
     """
@@ -95,10 +98,16 @@ def algorithm_1b(com_movie, counts_movie,ml_df, nf_df):
 
     eccentricity = 0.5
     for id in com_movie:
+        auxalso = []
         records_i = nf_df.loc[id,:]
         auxiliary_information_i = ml_df.loc[id,:]
         records = records_i.to_dict("records")
-        auxiliary_information = auxiliary_information_i.to_dict("records")
+        if isinstance(auxiliary_information_i, pandas.Series):
+            auxiliary_information = auxiliary_information_i.to_dict()
+            auxalso.append(auxiliary_information)
+            auxiliary_information = auxalso
+        else:
+            auxiliary_information = auxiliary_information_i.to_dict("records")
         for aux in auxiliary_information:
             scores = []
             #print(aux, "aux")
@@ -109,9 +118,9 @@ def algorithm_1b(com_movie, counts_movie,ml_df, nf_df):
                 score = score_function(aux, record, counts_movie.get(id))
                 scores.append(score)
             match, max_score, ecc = matching_criterion(scores, eccentricity)
-            pd = record_selection(scores)
             print("------------------------------------------")
             if match == True:
+                pd = record_selection(scores)
                 for i in range(len(scores)):
                     if scores[i] == max_score:
                         print("->Record:", i, "Score:", round(scores[i], 2), "Max Score:", round(max_score, 2),
@@ -122,9 +131,13 @@ def algorithm_1b(com_movie, counts_movie,ml_df, nf_df):
                               "Set Eccentricity:", eccentricity,"Calculated Eccentricity", round(ecc,2), "Probability:",
                               round(pd.get(i), 2))
                 print("\n")
-            else:
-                print("no match Eccentricity:", round(ecc,2), "<", eccentricity)
+            elif max_score == 0 and ecc == 0:
+                print("no match error in row!")
                 print("\n")
+            else:
+                print("no match Eccentricity:", round(ecc, 2), "<", eccentricity)
+                print("\n")
+
 
 
 
@@ -134,14 +147,14 @@ def algorithm_1b(com_movie, counts_movie,ml_df, nf_df):
 
 nf_df = pd.read_csv("C:\\Users\\jawwa\\OneDrive\\Studium\\Goethe Universität - BA\\7.Semester\\BA"
                     "\\BA-Implementierung\\datasets\\Netflix.csv",
-                     header=None, encoding="UTF-8", sep = ";", nrows=100000, skiprows=1)
+                     header=None, encoding="UTF-8", sep = ";", skiprows=1, nrows=3600)
 
-
+nf_df_dict = nf_df.to_dict("records")
 
 
 ml_df = pd.read_csv("C:\\Users\\jawwa\\OneDrive\\Studium\\Goethe Universität - BA\\"
                     "7.Semester\\BA\\BA-Implementierung\\datasets\\MovieLens.csv",
-                     header=None, encoding="UTF-8", sep = ";", nrows=100000, skiprows=1)
+                     header=None, encoding="UTF-8", sep = ";", skiprows=1, nrows=3000)
 
 
 
@@ -152,10 +165,9 @@ auxi_test  = [
     {0: '1', 1: '151', 2: '4.0', 3: '2171.0'},
     {0: '1', 1: '5167', 2: '5.0', 3: '2171.0'},
     {0: '1', 1: '5167', 2: '3.0', 3: '2312.0'},
-    {0: '1', 1: '5168', 2: '1.0', 3: '1175.0'},
     {0: '1', 1: '5168', 2: '4.0', 3: '2575.0'}
 ]
-
+#{0: '1', 1: '5168', 2: '1.0', 3: '1175.0'},
 
 #{0: 'userId', 1: 'rating', 2: 'timestamp', 3: 'movieId'},
 records_test = [
@@ -169,12 +181,18 @@ records_test = [
     {0: '2535052', 1: '4.0', 2: '2575.0', 3: '5168'},
     {0: '76196', 1: '1.0', 2: '1175.0', 3: '5168'}
 ]
-count_movie = ct(entry[3] for entry in records_test)
-deafen = pd.DataFrame(records_test)
-dafen = pd.DataFrame(auxi_test)
+
+#count_movie = ct(entry[3] for entry in records_test)
+count_movie = ct(entry[3] for entry in nf_df_dict)
+#deafen = pd.DataFrame(records_test)
+#dafen = pd.DataFrame(auxi_test)
+deafen = nf_df
+dafen = ml_df
 deafen.set_index([3],inplace=True) # setze movieID als index also haupterkennung
 dafen.set_index([1],inplace=True) # setze movieID als index also haupterkennung
 com_movies = deafen.index.unique().intersection(dafen.index.unique()) # gib nur die MovieIDs die beide haben
+
+
 
 algorithm_1b(com_movies, count_movie, dafen, deafen)
 
